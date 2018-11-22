f220c2a890150f35bdf91c359b3e4fc8b23e19ce
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-11-22 16:02:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -393,7 +393,6 @@ client.on('message', message => {
             message.guild.members.forEach(m => {
        if(!message.member.hasPermission('ADMINISTRATOR')) return;
                 var bc = new Discord.RichEmbed()
-                .addField(' »[user] :)
                 .addField(' » الرسالة : ', args)
                 .setColor('#ff0000')
                 // m.send(`[${m}]`);
